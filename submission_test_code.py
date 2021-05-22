945a5c30508b268a07da9eacc16dad8c28280042
Anssi "Miffyli" Kanervisto <kaneran21@hotmail.com>
2021-05-22 15:21:47 +0300

    Initial commit

---
+++ submission_test_code.py
@@ -0,0 +1,342 @@
+import numpy as np
+import gym
+import torch as th
+from torch import nn
+
+MAX_TEST_EPISODE_LEN = 18000  # 18k is the default for MineRLObtainDiamond.
+TREECHOP_STEPS = 2000  # number of steps to run BC lumberjack for in evaluations.
+
+TEST_MODEL_NAME = 'another_potato.pth'
+
+
+# !!! Do not change this! This is part of the submission kit !!!
+class EpisodeDone(Exception):
+    pass
+
+
+# !!! Do not change this! This is part of the submission kit !!!
+class Episode(gym.Env):
+    """A class for a single episode."""
+    def __init__(self, env):
+        self.env = env
+        self.action_space = env.action_space
+        self.observation_space = env.observation_space
+        self._done = False
+
+    def reset(self):
+        if not self._done:
+            return self.env.reset()
+
+    def step(self, action):
+        s, r, d, i = self.env.step(action)
+        if d:
+            self._done = True
+            raise EpisodeDone()
+        else:
+            return s, r, d, i
+
+class NatureCNN(nn.Module):
+    """
+    CNN from DQN nature paper:
+        Mnih, Volodymyr, et al.
+        "Human-level control through deep reinforcement learning."
+        Nature 518.7540 (2015): 529-533.
+    :param input_shape: A three-item tuple telling image dimensions in (C, H, W)
+    :param output_dim: Dimensionality of the output vector
+    """
+
+    def __init__(self, input_shape, output_dim):
+        super().__init__()
+        n_input_channels = input_shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with th.no_grad():
+            n_flatten = self.cnn(th.zeros(1, *input_shape)).shape[1]
+
+        self.linear = nn.Sequential(
+            nn.Linear(n_flatten, 512),
+            nn.ReLU(),
+            nn.Linear(512, output_dim)
+        )
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.linear(self.cnn(observations))
+
+
+class PovOnlyObservation(gym.ObservationWrapper):
+    """
+    Turns the observation space into POV only, ignoring the inventory. This is needed for stable_baselines3 RL agents,
+    as they don't yet support dict observations. The support should be coming soon (as of April 2021).
+    See following PR for details:
+    https://github.com/DLR-RM/stable-baselines3/pull/243
+    """
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = self.env.observation_space['pov']
+
+    def observation(self, observation):
+        return observation['pov']
+
+
+class ActionShaping(gym.ActionWrapper):
+    """
+    The default MineRL action space is the following dict:
+    Dict(attack:Discrete(2),
+         back:Discrete(2),
+         camera:Box(low=-180.0, high=180.0, shape=(2,)),
+         craft:Enum(crafting_table,none,planks,stick,torch),
+         equip:Enum(air,iron_axe,iron_pickaxe,none,stone_axe,stone_pickaxe,wooden_axe,wooden_pickaxe),
+         forward:Discrete(2),
+         jump:Discrete(2),
+         left:Discrete(2),
+         nearbyCraft:Enum(furnace,iron_axe,iron_pickaxe,none,stone_axe,stone_pickaxe,wooden_axe,wooden_pickaxe),
+         nearbySmelt:Enum(coal,iron_ingot,none),
+         place:Enum(cobblestone,crafting_table,dirt,furnace,none,stone,torch),
+         right:Discrete(2),
+         sneak:Discrete(2),
+         sprint:Discrete(2))
+    It can be viewed as:
+         - buttons, like attack, back, forward, sprint that are either pressed or not.
+         - mouse, i.e. the continuous camera action in degrees. The two values are pitch (up/down), where up is
+           negative, down is positive, and yaw (left/right), where left is negative, right is positive.
+         - craft/equip/place actions for items specified above.
+    So an example action could be sprint + forward + jump + attack + turn camera, all in one action.
+    This wrapper makes the action space much smaller by selecting a few common actions and making the camera actions
+    discrete. You can change these actions by changing self._actions below. That should just work with the RL agent,
+    but would require some further tinkering below with the BC one.
+    """
+    def __init__(self, env, camera_angle=10, always_attack=False):
+        super().__init__(env)
+
+        self.camera_angle = camera_angle
+        self.always_attack = always_attack
+        self._actions = [
+            [('attack', 1)],
+            [('forward', 1)],
+            # [('back', 1)],
+            # [('left', 1)],
+            # [('right', 1)],
+            # [('jump', 1)],
+            # [('forward', 1), ('attack', 1)],
+            # [('craft', 'planks')],
+            [('forward', 1), ('jump', 1)],
+            [('camera', [-self.camera_angle, 0])],
+            [('camera', [self.camera_angle, 0])],
+            [('camera', [0, self.camera_angle])],
+            [('camera', [0, -self.camera_angle])],
+        ]
+
+        self.actions = []
+        for actions in self._actions:
+            act = self.env.action_space.noop()
+            for a, v in actions:
+                act[a] = v
+            if self.always_attack:
+                act['attack'] = 1
+            self.actions.append(act)
+
+        self.action_space = gym.spaces.Discrete(len(self.actions))
+
+    def action(self, action):
+        return self.actions[action]
+
+
+def dataset_action_batch_to_actions(dataset_actions, camera_margin=5):
+    """
+    Turn a batch of actions from dataset (`batch_iter`) to a numpy
+    array that corresponds to batch of actions of ActionShaping wrapper (_actions).
+    Camera margin sets the threshold what is considered "moving camera".
+    Note: Hardcoded to work for actions in ActionShaping._actions, with "intuitive"
+        ordering of actions.
+        If you change ActionShaping._actions, remember to change this!
+    Array elements are integers corresponding to actions, or "-1"
+    for actions that did not have any corresponding discrete match.
+    """
+    # There are dummy dimensions of shape one
+    camera_actions = dataset_actions["camera"].squeeze()
+    attack_actions = dataset_actions["attack"].squeeze()
+    forward_actions = dataset_actions["forward"].squeeze()
+    jump_actions = dataset_actions["jump"].squeeze()
+    batch_size = len(camera_actions)
+    actions = np.zeros((batch_size,), dtype=np.int)
+
+    for i in range(len(camera_actions)):
+        # Moving camera is most important (horizontal first)
+        if camera_actions[i][0] < -camera_margin:
+            actions[i] = 3
+        elif camera_actions[i][0] > camera_margin:
+            actions[i] = 4
+        elif camera_actions[i][1] > camera_margin:
+            actions[i] = 5
+        elif camera_actions[i][1] < -camera_margin:
+            actions[i] = 6
+        elif forward_actions[i] == 1:
+            if jump_actions[i] == 1:
+                actions[i] = 2
+            else:
+                actions[i] = 1
+        elif attack_actions[i] == 1:
+            actions[i] = 0
+        else:
+            # No reasonable mapping (would be no-op)
+            actions[i] = -1
+    return actions
+
+
+def str_to_act(env, actions):
+    """
+    Simplifies specifying actions for the scripted part of the agent.
+    Some examples for a string with a single action:
+        'craft:planks'
+        'camera:[10,0]'
+        'attack'
+        'jump'
+        ''
+    There should be no spaces in single actions, as we use spaces to separate actions with multiple "buttons" pressed:
+        'attack sprint forward'
+        'forward camera:[0,10]'
+    :param env: base MineRL environment.
+    :param actions: string of actions.
+    :return: dict action, compatible with the base MineRL environment.
+    """
+    act = env.action_space.noop()
+    for action in actions.split():
+        if ":" in action:
+            k, v = action.split(':')
+            if k == 'camera':
+                act[k] = eval(v)
+            else:
+                act[k] = v
+        else:
+            act[action] = 1
+    return act
+
+
+def get_action_sequence():
+    """
+    Specify the action sequence for the scripted part of the agent.
+    """
+    # make planks, sticks, crafting table and wooden pickaxe:
+    action_sequence = []
+    action_sequence += [''] * 100
+    action_sequence += ['craft:planks'] * 4
+    action_sequence += ['craft:stick'] * 2
+    action_sequence += ['craft:crafting_table']
+    action_sequence += ['camera:[10,0]'] * 18
+    action_sequence += ['attack'] * 20
+    action_sequence += [''] * 10
+    action_sequence += ['jump']
+    action_sequence += [''] * 5
+    action_sequence += ['place:crafting_table']
+    action_sequence += [''] * 10
+
+    # bug: looking straight down at a crafting table doesn't let you craft. So we look up a bit before crafting.
+    action_sequence += ['camera:[-1,0]']
+    action_sequence += ['nearbyCraft:wooden_pickaxe']
+    action_sequence += ['camera:[1,0]']
+    action_sequence += [''] * 10
+    action_sequence += ['equip:wooden_pickaxe']
+    action_sequence += [''] * 10
+
+    # dig down:
+    action_sequence += ['attack'] * 600
+    action_sequence += [''] * 10
+
+    return action_sequence
+
+
+class MineRLAgent():
+    """
+    To compete in the competition, you are required to implement the two
+    functions in this class:
+        - load_agent: a function that loads e.g. network models
+        - run_agent_on_episode: a function that plays one game of MineRL
+
+    By default this agent behaves like a random agent: pick random action on
+    each step.
+
+    NOTE:
+        This class enables the evaluator to run your agent in parallel in Threads,
+        which means anything loaded in load_agent will be shared among parallel
+        agents. Take care when tracking e.g. hidden state (this should go to run_agent_on_episode).
+    """
+
+    def load_agent(self):
+        """
+        This method is called at the beginning of the evaluation.
+        You should load your model and do any preprocessing here.
+        THIS METHOD IS ONLY CALLED ONCE AT THE BEGINNING OF THE EVALUATION.
+        DO NOT LOAD YOUR MODEL ANYWHERE ELSE.
+        """
+        # Load up the behavioural cloning model
+        self.network = th.load(TEST_MODEL_NAME).cuda()
+
+    def run_agent_on_episode(self, single_episode_env: Episode):
+        """This method runs your agent on a SINGLE episode.
+
+        You should just implement the standard environment interaction loop here:
+            obs  = env.reset()
+            while not done:
+                env.step(self.agent.act(obs))
+                ...
+
+        NOTE:
+            This method will be called in PARALLEL during evaluation.
+            So, only store state in LOCAL variables.
+            For example, if using an LSTM, don't store the hidden state in the class
+            but as a local variable to the method.
+
+        Args:
+            env (gym.Env): The env your agent should interact with.
+        """
+        env = PovOnlyObservation(single_episode_env)
+        env = ActionShaping(single_episode_env, always_attack=True)
+        env1 = env.unwrapped
+
+        obs = env.reset()
+        done = False
+        total_reward = 0
+        steps = 0
+
+        num_actions = env.action_space.n
+        action_list = np.arange(num_actions)
+
+        action_sequence = get_action_sequence()
+
+        # BC part to get some logs:
+        for i in range(TREECHOP_STEPS):
+            # Process the action:
+            #   - Add/remove batch dimensions
+            #   - Transpose image (needs to be channels-last)
+            #   - Normalize image
+            obs = th.from_numpy(obs.transpose(2, 0, 1)[None].astype(np.float32) / 255).cuda()
+            # Turn logits into probabilities
+            probabilities = th.softmax(self.network(obs), dim=1)[0]
+            # Into numpy
+            probabilities = probabilities.detach().cpu().numpy()
+            # Sample action according to the probabilities
+            action = np.random.choice(action_list, p=probabilities)
+
+            obs, reward, done, info = env.step(action)
+            total_reward += reward
+            steps += 1
+            if done:
+                break
+
+        # scripted part to use the logs:
+        if not done:
+            for i, action in enumerate(action_sequence[:MAX_TEST_EPISODE_LEN - TREECHOP_STEPS]):
+                obs, reward, done, _ = env1.step(str_to_act(env1, action))
+                total_reward += reward
+                steps += 1
+                if done:
+                    break
